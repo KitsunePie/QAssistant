712d8b2a815fba6ed2bc5f0699a39b73d7ca99c4
KyuubiRan <KyuubiRan@qq.com>
2021-03-12 15:58:23 +0800

    Add HookUtils

---
+++ app/src/main/java/com/qnotifiedx/app/util/HookUtils.kt
@@ -0,0 +1,65 @@
+package com.qnotifiedx.app.util
+
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import java.lang.reflect.Method
+
+
+/**
+ * 扩展函数 hook方法
+ * @param hookCallback XC_MethodHook
+ */
+fun Method.hookMethod(hookCallback: XC_MethodHook) {
+    XposedBridge.hookMethod(this, hookCallback)
+}
+
+/**
+ * 扩展函数 hook方法执行前
+ * @param hook hook具体实现
+ */
+fun Method.hookBefore(hook: (XC_MethodHook.MethodHookParam) -> Unit) {
+    this.hookMethod(object : XC_MethodHook() {
+        override fun beforeHookedMethod(param: MethodHookParam?) {
+            hook(param!!)
+        }
+    })
+}
+
+/**
+ * 扩展函数 hook方法执行前
+ * @param hook hook具体实现
+ * @param priority 优先级 默认为0 即留空
+ */
+fun Method.hookBefore(hook: (XC_MethodHook.MethodHookParam) -> Unit, priority: Int = 0) {
+    this.hookMethod(object : XC_MethodHook(priority) {
+        override fun beforeHookedMethod(param: MethodHookParam?) {
+            hook(param!!)
+        }
+    })
+}
+
+/**
+ * 扩展函数 hook方法执行后
+ * @param hook hook具体实现
+ */
+fun Method.hookAfter(hook: (XC_MethodHook.MethodHookParam) -> Unit) {
+    this.hookMethod(object : XC_MethodHook() {
+        override fun afterHookedMethod(param: MethodHookParam?) {
+            hook(param!!)
+        }
+    })
+}
+
+/**
+ * 扩展函数 hook方法执行后
+ * @param hook hook具体实现
+ * @param priority 优先级 默认为0 即留空
+ */
+fun Method.hookAfter(hook: (XC_MethodHook.MethodHookParam) -> Unit, priority: Int) {
+    this.hookMethod(object : XC_MethodHook(priority) {
+        override fun afterHookedMethod(param: MethodHookParam?) {
+            hook(param!!)
+        }
+    })
+}
+
