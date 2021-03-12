5e383f9dfed7126855d3211497fb487f516290d0
KyuubiRan <KyuubiRan@qq.com>
2021-03-12 22:41:24 +0800

    Update HookInit

--- app/src/main/java/com/qnotifiedx/app/HookInit.kt
+++ app/src/main/java/com/qnotifiedx/app/HookInit.kt
@@ -1,10 +1,6 @@
 package com.qnotifiedx.app
 
-import android.content.Context
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.view.size
-import com.qnotifiedx.app.util.*
+import com.qnotifiedx.app.hook.base.BaseHook
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
@@ -22,38 +18,6 @@ class HookInit : IXposedHookLoadPackage {
     }
 
     private fun init() {
-        for (m in getMethods("com.tencent.mobileqq.activity.QQSettingSettingActivity")) {
-            if (m.name != "doOnCreate") continue
-            m.hookAfter {
-                val thisObject = it.thisObject
-                //加载QQ的设置物件类
-                val cFormSimpleItem = loadClass("com.tencent.mobileqq.widget.FormSimpleItem")
-                //获取所在的ViewGroup
-                val vg =
-                    (thisObject.getObjectOrNull("a", cFormSimpleItem) as View).parent as ViewGroup
-                //创建入口View
-                val entry = cFormSimpleItem.newInstance(
-                    arrayOf<Any>(thisObject as Context),
-                    arrayOf(Context::class.java)
-                ) as View
-                //设置入口属性
-                entry.apply {
-                    invokeMethod(
-                        "setLeftText",
-                        arrayOf("QNotifiedX"),
-                        arrayOf(CharSequence::class.java)
-                    )
-                    invokeMethod(
-                        "setRightText",
-                        arrayOf(BuildConfig.VERSION_NAME),
-                        arrayOf(CharSequence::class.java)
-                    )
-                    setOnClickListener { }
-                }
-                //添加入口
-                vg.addView(entry, (vg.size / 2) - 4)
-            }
-        }
+        BaseHook.initHooks()
     }
 }
-
