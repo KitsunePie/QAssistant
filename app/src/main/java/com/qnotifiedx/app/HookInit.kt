86a7aeb64ac44136247e7625dcfb73c50c2f871a
ketal178 <ketal@qq.com>
2021-03-12 16:26:43 +0800

    Update HookInit

--- app/src/main/java/com/qnotifiedx/app/HookInit.kt
+++ app/src/main/java/com/qnotifiedx/app/HookInit.kt
@@ -4,8 +4,8 @@ import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class HookInit : IXposedHookLoadPackage {
-    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
-        if (lpparam?.packageName == "com.tencent.mobileqq") {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (lpparam.packageName == "com.tencent.mobileqq") {
             TODO("HOOK START")
         }
     }
