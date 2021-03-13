bafd28b68b1b95372f168d53b73960d87bde3bac
KyuubiRan <KyuubiRan@qq.com>
2021-03-13 13:56:35 +0800

    Update GetAppContext

--- app/src/main/java/com/qnotifiedx/app/hook/normal/GetAppContext.kt
+++ app/src/main/java/com/qnotifiedx/app/hook/normal/GetAppContext.kt
@@ -15,10 +15,10 @@ object GetAppContext : BaseNormalHook() {
         private set
 
     override fun init() {
-        if (application != null) return
         for (m in getMethods("com.tencent.mobileqq.startup.step.LoadDex")) {
             if (m.returnType == Boolean::class.javaPrimitiveType && m.parameterTypes.isEmpty()) {
                 m.hookAfter {
+                    if (application != null) return@hookAfter
                     //加载QQ的基础Application
                     val cBaseApplicationImpl =
                         loadClass("com.tencent.common.app.BaseApplicationImpl")
