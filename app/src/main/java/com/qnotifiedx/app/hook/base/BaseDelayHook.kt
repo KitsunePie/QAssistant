2896ba91f058c6904e6657080c8ef32036118ad8
KyuubiRan <KyuubiRan@qq.com>
2021-03-13 13:24:07 +0800

    Add BaseDelayHook

---
+++ app/src/main/java/com/qnotifiedx/app/hook/base/BaseDelayHook.kt
@@ -0,0 +1,30 @@
+package com.qnotifiedx.app.hook.base
+
+import com.qnotifiedx.app.util.Log
+
+/**
+ * Delay Hooks
+ * 在模块加载完毕后延迟执行的Hook将继承于此类
+ */
+abstract class BaseDelayHook {
+    protected open var enable = false
+    protected var inited = false
+
+    companion object {
+        private val delayHooks = arrayOf<BaseDelayHook>(
+
+        )
+
+        fun initHooks() {
+            for (h in delayHooks) {
+                if (h.enable && !h.inited) {
+                    h.inited = true
+                    h.init()
+                    Log.i("Inited Delay hook:${h.javaClass.name}")
+                }
+            }
+        }
+    }
+
+    protected abstract fun init()
+}
