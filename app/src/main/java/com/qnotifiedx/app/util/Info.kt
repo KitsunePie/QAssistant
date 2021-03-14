6ccf362bc6fbd2652ec04adc6c948562e30a73db
KyuubiRan <KyuubiRan@qq.com>
2021-03-14 11:00:36 +0800

    Update Info.kt

--- app/src/main/java/com/qnotifiedx/app/util/Info.kt
+++ app/src/main/java/com/qnotifiedx/app/util/Info.kt
@@ -1,6 +1,7 @@
 package com.qnotifiedx.app.util
 
 import android.content.pm.PackageInfo
+import android.os.Build
 import com.qnotifiedx.app.BuildConfig
 
 /**
@@ -12,10 +13,12 @@ object Info {
     const val MODULE_PACKAGE_NAME = BuildConfig.APPLICATION_ID
     const val MODULE_VERSION_CODE = BuildConfig.VERSION_CODE
     const val MODULE_VERSION_NAME = BuildConfig.VERSION_NAME
-
+    
     val HOST_PACKAGE_NAME: String? = appContext?.packageName
+
     @Suppress("DEPRECATION")
-    val HOST_VERSION_CODE: Int? = getHostPackageInfo()?.versionCode
+    val HOST_VERSION_CODE =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) getHostPackageInfo()?.longVersionCode else getHostPackageInfo()?.versionCode?.toLong()
     val HOST_VERSION_NAME: String? = getHostPackageInfo()?.versionName
 
     private fun getHostPackageInfo(): PackageInfo? {
