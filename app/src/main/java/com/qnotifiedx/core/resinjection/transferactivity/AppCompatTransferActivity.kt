8e276aaf8924d92618d3d997b7579ae11bffacce
KyuubiRan <KyuubiRan@qq.com>
2021-03-15 08:53:33 +0800

    Add comments

--- app/src/main/java/com/qnotifiedx/core/resinjection/transferactivity/AppCompatTransferActivity.kt
+++ app/src/main/java/com/qnotifiedx/core/resinjection/transferactivity/AppCompatTransferActivity.kt
@@ -3,6 +3,9 @@ package com.qnotifiedx.core.resinjection.transferactivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * 所有在宿主内启动的AppCompatActivity都应该继承与此类 否则会报错
+ */
 open class AppCompatTransferActivity : AppCompatActivity() {
     override fun getClassLoader(): ClassLoader {
         return AppCompatTransferActivity::class.java.classLoader!!
