a7474313a1c23a239f5940a6a13b9429f3d753d6
KyuubiRan <KyuubiRan@qq.com>
2021-03-15 10:52:43 +0800

    Update CustomSplash.kt

--- app/src/main/java/com/qnotifiedx/app/hook/normal/CustomSplash.kt
+++ app/src/main/java/com/qnotifiedx/app/hook/normal/CustomSplash.kt
@@ -6,25 +6,6 @@ import com.qnotifiedx.app.hook.base.BaseNormalHook
 //TEST ONLY
 @NormalHookEntry
 object CustomSplash : BaseNormalHook() {
-    private val TRANSPARENT_PNG = byteArrayOf(
-        0x89.toByte(), 0x50.toByte(), 0x4E.toByte(), 0x47.toByte(),
-        0x0D.toByte(), 0x0A.toByte(), 0x1A.toByte(), 0x0A.toByte(),
-        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x0D.toByte(),
-        0x49.toByte(), 0x48.toByte(), 0x44.toByte(), 0x52.toByte(),
-        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x01.toByte(),
-        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x01.toByte(),
-        0x08.toByte(), 0x06.toByte(), 0x00.toByte(), 0x00.toByte(),
-        0x00.toByte(), 0x1F.toByte(), 0x15.toByte(), 0xC4.toByte(),
-        0x89.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
-        0x0B.toByte(), 0x49.toByte(), 0x44.toByte(), 0x41.toByte(),
-        0x54.toByte(), 0x08.toByte(), 0xD7.toByte(), 0x63.toByte(),
-        0x60.toByte(), 0x00.toByte(), 0x02.toByte(), 0x00.toByte(),
-        0x00.toByte(), 0x05.toByte(), 0x00.toByte(), 0x01.toByte(),
-        0xE2.toByte(), 0x26.toByte(), 0x05.toByte(), 0x9B.toByte(),
-        0x00.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
-        0x49.toByte(), 0x45.toByte(), 0x4E.toByte(), 0x44.toByte(),
-        0xAE.toByte(), 0x42.toByte(), 0x60.toByte(), 0x82.toByte()
-    )
     override val name: String = "测试HOOK"
 
     override fun init() {
