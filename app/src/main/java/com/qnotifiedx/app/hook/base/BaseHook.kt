622a7ccfb86f1949c4e52658deee4cdc07229688
KyuubiRan <KyuubiRan@qq.com>
2021-03-15 10:36:43 +0800

    Update BaseHook.kt

--- app/src/main/java/com/qnotifiedx/app/hook/base/BaseHook.kt
+++ app/src/main/java/com/qnotifiedx/app/hook/base/BaseHook.kt
@@ -1,11 +1,30 @@
 package com.qnotifiedx.app.hook.base
 
+import android.content.Context
+import com.qnotifiedx.app.util.appContext
+
 /**
  * Hook基类
  */
 abstract class BaseHook {
-    protected open var enable = false
+    companion object {
+        private const val HOOK_CFG_NAME = "qnotifiedx_prefs"
+
+    }
+
+    abstract val name: String
+
     protected var inited = false
 
     protected abstract fun init()
+
+    open var enable: Boolean
+        set(value) {
+            appContext!!.getSharedPreferences(HOOK_CFG_NAME, Context.MODE_PRIVATE).edit()
+                .putBoolean(this::class.java.name, value).apply()
+        }
+        get() {
+            return appContext!!.getSharedPreferences(HOOK_CFG_NAME, Context.MODE_PRIVATE)
+                .getBoolean(this::class.java.name, false)
+        }
 }
