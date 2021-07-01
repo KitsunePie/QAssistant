12299797a4a84cf7b044bb1e95476a89250938b7
singleNeuron <cryolitia@gmail.com>
2021-07-01 16:08:06 +0800

    Import EzXHelper as a submodule
    Signed-off-by: singleNeuron <Cryolitia@gmail.com>

--- app/src/main/java/org/kitsunepie/qassistant/app/ui/module/fragment/DevsFragment.kt
+++ app/src/main/java/org/kitsunepie/qassistant/app/ui/module/fragment/DevsFragment.kt
@@ -1,15 +1,15 @@
 package org.kitsunepie.qassistant.app.ui.module.fragment
 
+import com.github.kyuubiran.ezxhelper.init.InitFields.moduleRes
 import org.kitsunepie.maitungtmui.base.UiScreen
 import org.kitsunepie.maitungtmui.base.uiCategory
 import org.kitsunepie.maitungtmui.base.uiClickableItem
 import org.kitsunepie.maitungtmui.base.uiScreen
 import org.kitsunepie.qassistant.R
-import org.kitsunepie.qassistant.app.ui.module.activity.safeResources
 import org.kitsunepie.qassistant.app.util.Utils.openUrl
 
 val devsFragment: UiScreen = uiScreen {
-    name = safeResources.getString(R.string.module_more_setting_about_main_devs)
+    name = moduleRes.getString(R.string.module_more_setting_about_main_devs)
     summary = null
     contains = linkedMapOf(
         uiCategory {
@@ -17,9 +17,9 @@ val devsFragment: UiScreen = uiScreen {
             contains = linkedMapOf(
                 uiClickableItem {
                     title =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_kyuubiran)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_kyuubiran)
                     summary =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_dev_summary)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_dev_summary)
                     onClickListener = {
                         it.openUrl("https://github.com/KyuubiRan")
                         true
@@ -27,9 +27,9 @@ val devsFragment: UiScreen = uiScreen {
                 },
                 uiClickableItem {
                     title =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_ketal)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_ketal)
                     summary =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_dev_summary)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_dev_summary)
                     onClickListener = {
                         it.openUrl("https://github.com/keta1")
                         true
@@ -37,9 +37,9 @@ val devsFragment: UiScreen = uiScreen {
                 },
                 uiClickableItem {
                     title =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_singleneuron)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_singleneuron)
                     summary =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_dev_summary)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_dev_summary)
                     onClickListener = {
                         it.openUrl("https://github.com/singleNeuron")
                         true
@@ -47,9 +47,9 @@ val devsFragment: UiScreen = uiScreen {
                 },
                 uiClickableItem {
                     title =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_nextalone)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_nextalone)
                     summary =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_dev_summary)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_dev_summary)
                     onClickListener = {
                         it.openUrl("https://github.com/NextAlone")
                         true
@@ -58,9 +58,9 @@ val devsFragment: UiScreen = uiScreen {
                 },
                 uiClickableItem {
                     title =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_maitung)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_maitung)
                     summary =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_ui_summary)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_ui_summary)
                     onClickListener = {
                         it.openUrl("https://github.com/Lagrio")
                         true
@@ -74,7 +74,7 @@ val devsFragment: UiScreen = uiScreen {
             contains = linkedMapOf(
                 uiClickableItem {
                     title =
-                        safeResources.getString(R.string.module_more_setting_about_main_devs_join_us)
+                        moduleRes.getString(R.string.module_more_setting_about_main_devs_join_us)
                 }
             )
         }
--- app/src/main/java/org/kitsunepie/qassistant/app/ui/module/fragment/ModuleSettingPage.kt
+++ app/src/main/java/org/kitsunepie/qassistant/app/ui/module/fragment/ModuleSettingPage.kt
@@ -1,24 +1,24 @@
 package org.kitsunepie.qassistant.app.ui.module.fragment
 
+import com.github.kyuubiran.ezxhelper.init.InitFields.moduleRes
 import org.kitsunepie.maitungtmui.base.UiScreen
 import org.kitsunepie.maitungtmui.base.uiCategory
 import org.kitsunepie.maitungtmui.base.uiScreen
 import org.kitsunepie.maitungtmui.base.uiSwitchItem
 import org.kitsunepie.qassistant.R
-import org.kitsunepie.qassistant.app.ui.module.activity.safeResources
 import org.kitsunepie.qassistant.core.config.Config
 import org.kitsunepie.qassistant.core.config.ModuleConfig
 
 val moduleSettingFragment: UiScreen = uiScreen {
-    name = safeResources.getString(R.string.module_other_setting_module)
+    name = moduleRes.getString(R.string.module_other_setting_module)
     summary = null
     contains = linkedMapOf(
         uiCategory {
-            name = safeResources.getString(R.string.module_other_setting_module_startup)
+            name = moduleRes.getString(R.string.module_other_setting_module_startup)
             contains = linkedMapOf(
                 uiSwitchItem {
                     title =
-                        safeResources.getString(R.string.module_other_setting_module_startup_toast)
+                        moduleRes.getString(R.string.module_other_setting_module_startup_toast)
                     value.value = Config.sModulePref.getBoolean(ModuleConfig.M_STARTUP_TOAST, false)
                     value.observeForever {
                         Config.sModulePref.putBoolean(ModuleConfig.M_STARTUP_TOAST, it)
