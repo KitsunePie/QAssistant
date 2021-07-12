8e0659d89c2d5622b58360d561e59e74e836a98b
KyuubiRan <KyuubiRan@qq.com>
2021-07-12 16:45:21 +0800

    Fixed PreventUpdate

--- app/src/main/java/org/kitsunepie/qassistant/app/hook/normal/function/PreventUpdate.kt
+++ app/src/main/java/org/kitsunepie/qassistant/app/hook/normal/function/PreventUpdate.kt
@@ -1,7 +1,6 @@
 package org.kitsunepie.qassistant.app.hook.normal.function
 
 import com.github.kyuubiran.ezxhelper.utils.findMethodByCondition
-import com.github.kyuubiran.ezxhelper.utils.isStatic
 import org.kitsunepie.qassistant.annotations.NormalHookEntry
 import org.kitsunepie.qassistant.app.hook.base.BaseSwitchHook
 import org.kitsunepie.qassistant.app.util.ClassPointer
@@ -15,7 +14,7 @@ object PreventUpdate : BaseSwitchHook() {
     override fun init() {
         val clz = ClassPointer.UpgradeController1.clazz ?: ClassPointer.UpgradeController2.clazz!!
         findMethodByCondition(clz) {
-            it.name == "a" && !it.isStatic && it.parameterTypes.isEmpty()
+            it.name == "a" && it.returnType.name.contains("UpgradeDetailWrapper") && it.parameterTypes.isEmpty()
         }.hookBefore(this) {
             it.result = null
         }
