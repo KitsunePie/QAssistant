2e6deedfda59ea2bf3022c366d99a7443f0d609f
Kitsune <45789471+KyuubiRan@users.noreply.github.com>
2021-12-26 20:26:38 +0800

    Init compose (#41)

---
+++ app/src/main/java/org/kitsunepie/qassistant/app/ui/ModuleSetting.kt
@@ -0,0 +1,57 @@
+/*
+ * QAssistant - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2021
+ * https://github.com/KitsunePie/QAssistant
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation and our eula published by us;
+ *  either version 3 of the License, or any later version and our eula as published
+ * by us.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/KitsunePie/QAssistant/blob/master/LICENSE.md>.
+ */
+
+package org.kitsunepie.qassistant.app.ui
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.core.view.WindowCompat
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.kitsunepie.qassistant.app.ui.scene.MainScene
+
+
+object App {
+    private const val Main = "main"
+
+    @Composable
+    fun MainView() {
+        val nav = rememberNavController()
+        NavHost(navController = nav, startDestination = Main) {
+            composable(Main) {
+                MainScene(nav)
+            }
+        }
+    }
+}
+
+class ModuleSetting : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            App.MainView()
+        }
+    }
+}
