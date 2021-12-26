afae17dc236fd099a2ad6f383e0f45fa2563cfe4
KyuubiRan <KyuubiRan@qq.com>
2021-12-26 20:20:43 +0800

    Init compose

---
+++ app/src/main/java/org/kitsunepie/qassistant/app/ui/BaseActivity.kt
@@ -0,0 +1,46 @@
+/*
+ * QAssistant - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2021
+ * https://github.com/KitsunePie/QAssistant
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation and our eula published by us;
+ *  either version 3 of the License, or any later version and our eula as published
+ * by us.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/KitsunePie/QAssistant/blob/master/LICENSE.md>.
+ */
+
+package org.kitsunepie.qassistant.app.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import org.kitsunepie.qassistant.R
+
+open class BaseActivity : ComponentActivity() {
+    private val mLoder by lazy { BaseActivity::class.java.classLoader }
+
+    override fun getClassLoader(): ClassLoader = mLoder
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        val windowState = savedInstanceState.getBundle("android:viewHierarchyState")
+        if (windowState != null) {
+            windowState.classLoader = mLoder
+        }
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+}
